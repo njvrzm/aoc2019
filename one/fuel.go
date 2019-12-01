97946c0689f2c5c1479bbb5e7525b16c2f56a7f5
Nathan Verzemnieks <njvrzm@gmail.com>
2019-11-30 21:57:20 -0800

    Day one star two

--- one/fuel.go
+++ one/fuel.go
@@ -11,3 +11,22 @@ func TotalFuelForMasses(masses []int) int {
 	}
 	return total
 }
+
+func FuelForMassAndFuel(mass int) int {
+	total := 0
+	// 9 is the smallest mass that requires positive fuel: 9/3 - 2 == 1
+	for mass >= 9 {
+		fuel := FuelForMass(mass)
+		total += fuel
+		mass = fuel
+	}
+	return total
+}
+
+func TotalFuelForMassesAndFuels(masses []int) int {
+	total := 0
+	for _, mass := range masses {
+		total += FuelForMassAndFuel(mass)
+	}
+	return total
+}
--- one/fuel_test.go
+++ one/fuel_test.go
@@ -18,16 +18,40 @@ func TestFuelForMass(t *testing.T) {
 	for _, massFuel := range massFuels {
 		required := FuelForMass(massFuel.mass)
 		if required != massFuel.fuel {
-			t.Errorf("Fuel required for %d should be %d; got %d", massFuel.mass, massFuel.fuel, required)
+			t.Errorf("Fuel required for mass of %d should be %d; got %d", massFuel.mass, massFuel.fuel, required)
 		}
 	}
 }
 
-func TestTotalFuelForInput(t *testing.T) {
+func TestFuelForMassAndFuel(t *testing.T) {
+	var massFuels = []struct {
+		mass, fuel int
+	}{
+		{14, 2},
+		{1969, 966},
+		{100756, 50346},
+	}
+	for _, massFuel := range massFuels {
+		required := FuelForMassAndFuel(massFuel.mass)
+		if required != massFuel.fuel {
+			t.Errorf("Fuel required for mass of %d and subsequent fuels should be %d; got %d", massFuel.mass, massFuel.fuel, required)
+		}
+	}
+}
+
+func TestTotalFuelForMassForInput(t *testing.T) {
 	masses := util.NumbersFromFile("testdata/input.txt")
 	total := TotalFuelForMasses(masses)
 	expected := 3452245
 	if total != expected {
 		t.Errorf("Total fuel required should be %d", expected)
+	}
+}
+func TestTotalFuelForMassesAndFuelsForInput(t *testing.T) {
+	masses := util.NumbersFromFile("testdata/input.txt")
+	total := TotalFuelForMassesAndFuels(masses)
+	expected := 5175499
+	if total != expected {
+		t.Errorf("Total fuel required should be %d, got %d", expected, total)
 	}
 }
